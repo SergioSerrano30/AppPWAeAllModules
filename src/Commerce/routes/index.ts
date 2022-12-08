89f7cbb60d098469ac54830bd6c2ab8b53761c5f
Zambrano <emmanuel@gmail.com>
2022-12-07 20:03:47 -0700

    push 20,00

--- src/Commerce/routes/index.ts
+++ src/Commerce/routes/index.ts
@@ -45,7 +45,7 @@ const routes: Routes = {
     path: '*',
   },
   [Pages.Carrito]: {
-    component: asyncComponentLoader(() => import('@/Commerce/pages/orders/Carrito/Carrito')),
+    component: asyncComponentLoader(() => import('@/Commerce/pages/orders/CheckOut/CheckOut')),
     path: '/carrito',
     title: 'Carrito',
     icon: LocalGroceryStoreIcon,
